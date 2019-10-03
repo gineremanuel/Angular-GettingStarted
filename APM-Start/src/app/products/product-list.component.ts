a88339f1ad26976177b9de70f4b2fe21757e1ce1
Emanuel Giner <emanuelginer77@gmail.com>
2019-10-03 17:05:28 -0300

    star component

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -74,6 +74,11 @@ export class ProductListComponent implements OnInit {
     constructor() {
         this.filteredProducts = this.products;
     }
+
+    onRatingClicked(message: string): void {
+        this.pageTitle = 'Product List: ' + message;
+    }
+
     performFilter(filterBy: string): IProduct[] {
         filterBy = filterBy.toLocaleLowerCase();
         return this.products.filter((product: IProduct) =>
